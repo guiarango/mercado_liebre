a523aa63eec76dd3e92d709cdba924a48dc025e2
guiarango <guiarango@hotmail.com>
2021-12-08 20:49:39 -0500

    Se sube el proyecto de Mercado liebre a GIT

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const pathPublic = path.resolve(__dirname, "./public");
+
+app.use(express.static(pathPublic));
+
+app.listen(3000, () =>
+  console.log("Servidor está corriendo en el puerto 3000")
+);
+
+app.get("/", (req, res) => {
+  let pathNew = path.resolve(__dirname, "views/home.html");
+  res.sendFile(pathNew);
+});
+
+app.get("/register", (req, res) => {
+  let pathNew = path.resolve(__dirname, "views/register.html");
+  res.sendFile(pathNew);
+});
+
+app.get("/login", (req, res) => {
+  let pathNew = path.resolve(__dirname, "views/login.html");
+  res.sendFile(pathNew);
+});
